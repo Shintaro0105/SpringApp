f300cf7a0834cba5af8231354bfaead64bec49a9
Shintaro0105 <shintarou0105@gmail.com>
2025-08-17 05:53:34 +0000

    make mapperxml usable

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 //import SpringLogin.app.service.UserService;
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -3,13 +3,11 @@ package com.example.demo.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.dao.DataAccessException;
 
 import com.example.demo.model.User;
 
 @Mapper
 public interface UserMapper {
-    @Select("SELECT * FROM user")
     List<User> selectMany() throws DataAccessException;
 }
